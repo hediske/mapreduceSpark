771764fc4e0832f6559a067d9a10fbc0ef367ec5
hediske <mohamedhedi.aissi@yahoo.fr>
2024-12-04 18:12:17 +0100

    fixed init script

--- init-script.sh
+++ init-script.sh
@@ -2,17 +2,13 @@
 
 cd wordcount-mapreduce
 mvn clean package
-mv target/wordcount-mapreduce-1.0-SNAPSHOT.jar ../mapreduce.jar
+mv target/wordcount-1.0-SNAPSHOT.jar ../mapreduce.jar
 
 cd ../wordcount-spark
 mvn clean package
-mv target/wordcount-spark-1.0-SNAPSHOT.jar ../spark.jar
+mv target/spark-wordcount-1.0-SNAPSHOT.jar ../spark.jar
 
 
 docker compose up -d
-docker cp input.txt namenode:/input.txt
-docker exec namenode hdfs dfs -mkdir -p /user/hadoop/input
-docker exec namenode hadoop dfs -rm -r /user/hadoop/input > /dev/null 2>&1
-docker exec namenode hdfs dfs -put /input.txt /user/hadoop/input
 
 
--- uploadToHadoop.sh
+++ uploadToHadoop.sh
@@ -1,5 +1,6 @@
 #!bin/bash 
 
+docker cp input.txt namenode:/input.txt
 docker exec namenode hdfs dfs -mkdir -p /user/hadoop/input
 docker exec namenode hadoop dfs -rm -r /user/hadoop/input > /dev/null 2>&1
 docker exec namenode hdfs dfs -put /input.txt /user/hadoop/input
